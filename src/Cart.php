3c767470aadb44d4d639fbb1852f97305f16b583
Lucas Yang <yangchenshin77@gmail.com>
2019-12-10 14:04:49 +0800

    Add Product class

--- src/Cart.php
+++ src/Cart.php
@@ -4,20 +4,34 @@ namespace Ycs77\DesignPattern;
 
 class Cart
 {
+    /** @var \Ycs77\DesignPattern\Product[] */
+    protected $items = [];
+
     /** @var \Ycs77\DesignPattern\CashFlow */
     protected $cashFlow;
 
     /** @var int */
     protected $total;
 
-    public function __construct(CashFlow $cashFlow)
+    public function __construct(array $items, CashFlow $cashFlow)
     {
+        $this->items = $items;
         $this->cashFlow = $cashFlow;
     }
 
     public function setTotal(int $total)
     {
         $this->total = $total;
+    }
+
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    public function getDiscountTotal()
+    {
+        return $this->total;
     }
 
     public function checkout()
