f367943f7b5b266444266cbc5ac6c04c4fe8a293
Lucas Yang <yangchenshin77@gmail.com>
2019-12-10 14:56:39 +0800

    Add decorator pattern tests

---
+++ tests/DecoratorPatternTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Ycs77\DesignPattern\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class DecoratorPatternTest extends TestCase
+{
+    /**
+     * 打7折 (全部商品).
+     */
+    public function testCalculatePercentOffDiscountPrice()
+    {
+        $discount = new Discount(1000);
+        $discount = new PercentOffDiscount($discount, 0.7);
+
+        $this->assertEquals(700, $discount->calculatePrice());
+    }
+
+    /**
+     * 滿 2000 優惠 100 (全部商品).
+     */
+    public function testCalculateOverDiscountPrice()
+    {
+        $discount = new Discount(2400);
+        $discount = new OverDiscount($discount, 2000, 100);
+
+        $this->assertEquals(2300, $discount->calculatePrice());
+    }
+
+    /**
+     * 萬聖節優惠 打9折 (全部商品).
+     */
+    public function testCalculateHalloweenDiscountPrice()
+    {
+        $discount = new Discount(1000);
+        $discount = new HalloweenDiscount($discount);
+
+        $this->assertEquals(900, $discount->calculatePrice());
+    }
+
+    /**
+     * 第2件6折 (限同商品).
+     */
+    public function testCalculatePercentOffSecondItemDiscountPrice()
+    {
+        $discount = new ProductDiscount(1000, 2);
+        $discount = new PercentOffSecondItemDiscount($discount, 0.6);
+
+        $this->assertEquals(1600, $discount->calculatePrice());
+    }
+
+    /**
+     * 第2件6折 買3件 (限同商品).
+     */
+    public function testCalculatePercentOffSecondItemDiscountPriceAndBuyThreeItems()
+    {
+        $discount = new ProductDiscount(1000, 3);
+        $discount = new PercentOffSecondItemDiscount($discount, 0.6);
+
+        $this->assertEquals(2600, $discount->calculatePrice());
+    }
+
+    /**
+     * 買1送1 (限同商品).
+     */
+    public function testCalculateBuyXGetXDiscountPrice()
+    {
+        $discount = new ProductDiscount(1000, 2);
+        $discount = new BuyXGetXDiscount($discount, 1, 1);
+
+        $this->assertEquals(1000, $discount->calculatePrice());
+    }
+
+    /**
+     * 買1送1 買3件 (限同商品).
+     */
+    public function testCalculateBuyXGetXDiscountPriceAndBuyThreeItems()
+    {
+        $discount = new ProductDiscount(1000, 3);
+        $discount = new BuyXGetXDiscount($discount, 1, 1);
+
+        $this->assertEquals(2000, $discount->calculatePrice());
+    }
+
+    /**
+     * 組合折扣.
+     *
+     * (全部商品)
+     * 打8折 + 萬聖節優惠 打9折 + 滿 800 優惠 50
+     */
+    public function testCalculateGroupDiscountPrice()
+    {
+        $discount = new Discount(3000);
+        $discount = new PercentOffDiscount($discount, 0.8);
+        $discount = new HalloweenDiscount($discount);
+        $discount = new OverDiscount($discount, 2000, 100);
+
+        $this->assertEquals(2060, $discount->calculatePrice());
+    }
+}
---
+++ tests/Integration/DecoratorPatternTest.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace Ycs77\DesignPattern\Test\Integration;
+
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+use Ycs77\DesignPattern\Cart;
+use Ycs77\DesignPattern\CashFlow;
+use Ycs77\DesignPattern\Product;
+
+class DecoratorPatternTest extends TestCase
+{
+    /** @var \Mockery\MockInterface|\Mockery\LegacyMockInterface|\Ycs77\DesignPattern\CashFlow $cashFlow */
+    protected $cashFlow;
+
+    /** @var \Ycs77\DesignPattern\Product[] */
+    protected $products;
+
+    protected function setUp(): void
+    {
+        $this->cashFlow = m::mock(CashFlow::class);
+
+        /** @var \Mockery\MockInterface|\Mockery\LegacyMockInterface $cashFlowproduct_1 */
+        $product_1 = m::mock(CashFlow::class);
+        $product_1->shouldReceive('getPrice')
+            ->andReturn(120)
+            ->once();
+        $product_1->shouldReceive('getCount')
+            ->andReturn(1)
+            ->once();
+
+        /** @var \Mockery\MockInterface|\Mockery\LegacyMockInterface $product_2 */
+        $product_2 = m::mock(Product::class);
+        $product_2->shouldReceive('getPrice')
+            ->andReturn(1000)
+            ->once();
+        $product_2->shouldReceive('getCount')
+            ->andReturn(3)
+            ->once();
+
+        /** @var \Mockery\MockInterface|\Mockery\LegacyMockInterface $product_3 */
+        $product_3 = m::mock(Product::class);
+        $product_3->shouldReceive('getPrice')
+            ->andReturn(89)
+            ->once();
+        $product_3->shouldReceive('getCount')
+            ->andReturn(2)
+            ->once();
+
+        $this->products = [
+            $product_1,
+            $product_2,
+            $product_3,
+        ];
+    }
+
+    /**
+     * 打7折 (全部商品).
+     */
+    public function testGetPercentOffDiscountTotal()
+    {
+        $cart = new Cart($this->products, $this->cashFlow);
+
+        $cart->setDiscount(function ($total) {
+            $discount = new Discount($total);
+            $discount = new PercentOffDiscount($discount, 0.7);
+
+            return $discount;
+        });
+
+        $cart->discount();
+
+        $this->assertEquals(2309, $cart->getTotal());
+    }
+
+    /**
+     * 滿 2000 優惠 100 (全部商品).
+     */
+    public function testGetOverDiscountTotal()
+    {
+        $cart = new Cart($this->products, $this->cashFlow);
+
+        $cart->setDiscount(function ($total) {
+            $discount = new Discount($total);
+            $discount = new OverDiscount($discount, 2000, 100);
+
+            return $discount;
+        });
+
+        $cart->discount();
+
+        $this->assertEquals(2209, $cart->getTotal());
+    }
+
+    /**
+     * 萬聖節優惠 打9折 (全部商品).
+     */
+    public function testGetHalloweenDiscountTotal()
+    {
+        $cart = new Cart($this->products, $this->cashFlow);
+
+        $cart->setDiscount(function ($total) {
+            $discount = new Discount($total);
+            $discount = new HalloweenDiscount($discount);
+
+            return $discount;
+        });
+
+        $cart->discount();
+
+        $this->assertEquals(2968, $cart->getTotal());
+    }
+
+    /**
+     * 第2件6折 (限同商品).
+     */
+    public function testGetPercentOffSecondItemDiscountTotal()
+    {
+        $cart = new Cart($this->products, $this->cashFlow);
+
+        $cart->setProductDiscount(function ($price, $count) {
+            $discount = new ProductDiscount($price, $count);
+            $discount = new PercentOffSecondItemDiscount($discount, 0.6);
+
+            return $discount;
+        });
+
+        $cart->discount();
+
+        $this->assertEquals(2862, $cart->getTotal());
+    }
+
+    /**
+     * 買1送1 (限同商品).
+     */
+    public function testGetBuyXGetXDiscountTotal()
+    {
+        $cart = new Cart($this->products, $this->cashFlow);
+
+        $cart->setProductDiscount(function ($price, $count) {
+            $discount = new ProductDiscount($price, $count);
+            $discount = new BuyXGetXDiscount($discount, 1, 1);
+
+            return $discount;
+        });
+
+        $cart->discount();
+
+        $this->assertEquals(2209, $cart->getTotal());
+    }
+
+    /**
+     * 組合折扣.
+     *
+     * (全部商品)
+     * 打8折 + 萬聖節優惠 打9折 + 滿 800 優惠 50
+     * +
+     * (限同商品)
+     * 買2送1
+     */
+    public function testGetGroupDiscountTotal()
+    {
+        $cart = new Cart($this->products, $this->cashFlow);
+
+        $cart->setDiscount(function ($total) {
+            $discount = new Discount($total);
+            $discount = new PercentOffDiscount($discount, 0.8);
+            $discount = new HalloweenDiscount($discount);
+            $discount = new OverDiscount($discount, 800, 50);
+
+            return $discount;
+        });
+
+        $cart->setProductDiscount(function ($price, $count) {
+            $discount = new ProductDiscount($price, $count);
+            $discount = new BuyXGetXDiscount($discount, 2, 1);
+
+            return $discount;
+        });
+
+        $cart->discount();
+
+        $this->assertEquals(1540, $cart->getTotal());
+    }
+}
